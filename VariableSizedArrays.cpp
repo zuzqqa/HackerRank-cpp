009ae1d6824cb74f279f0531e1b61fa21e0c1d49
zuzqqa <76653705+zuzqqa@users.noreply.github.com>
2022-11-17 22:21:49 +0100

    Update VariableSizedArrays.cpp

--- VariableSizedArrays.cpp
+++ VariableSizedArrays.cpp
@@ -1,5 +1,3 @@
-#include <cmath>
-#include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
@@ -8,29 +6,33 @@ using namespace std;
 
 
 int main() {
-    int n, q, p, k, i, j, suma = 0;
+    int n, q, p, k, i, j, suma;
     
-    cin>>n>>q; //liczba polece, liczba tablic
+    scanf("%d" "%d", &n, &q);
     
-    vector <int> A; //wektor z tablicami
-    vector <int> B; //wektor liczby tablicy i ich wymiarow
+    vector <int> A, B; 
     
-    for( int q; q > 0; q--){
-        cin>>k;
+    for(q; q > 0; q--){
+        scanf("%d", &k);
         B.push_back(k);
-        for( int i = 0; i < k; i++){
-        cin>>p;
+        for(i = 0; i < k; i++){
+        scanf("%d", &p);
         A.push_back(p);
         }
     }
     
-    for( int n; n > 0; n--){ 
-        cin >> i >> j;
+    for(n; n > 0; n--){ 
+        scanf("%d" "%d", &i, &j);
         suma = 0;
-        for( int o = 0; o <= i; o++){
+        if(i == 0){
+            suma = 0;
+        }
+        else{
+        for( int o = 0; o < i; o++){
             suma += B[o];
         }
-        cout<<A[suma + j]<<endl;
+        }
+        printf("%d\n",A[suma+j]);
     }
     return 0;
 }
